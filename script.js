d6c988e1ae22a433ad51a1b74a884b383eb97e1f
LenitoArruda <lenitoarruda@hotmail.com>
2022-08-04 22:02:29 -0300

    Javascrip functions

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 
-function insertNumber(number){
-    const input = document.querySelector("#txt-valor");
-    if(input.value==="0")
-         input.value = number
-    
-    else
-    input.value += number
-    
+const result = document.querySelector("#txt-valor");
+let firstValue = 0;
+let secondValue = 0;
+let operation = '';
 
+
+// Insert the numbers into the txt-valor 
+function insertNumber(number){
+    if(result.value==="0") 
+    result.value = number
+    else 
+    result.value += number  
+}
+
+// Applying math operations
+function mathOperations(op){
+    firstValue = result.value;
+    operation = op;
+    
+}
+
+function resultOperation(){
+    secondValue = result.value;
+    switch(operation){
+        case '*' : 
+            result.value = firstValue * secondValue;
+            break;
+        case '/' :
+             result.value = firstValue / secondValue;
+             break;
+        case '-' : 
+            result.value = firstValue - secondValue;
+            break;
+        case '+' : 
+            result.value = (firstValue + secondValue);
+    }
 }
